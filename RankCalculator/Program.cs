ca924b5373d11cff763bad86ab5b9756d0b82619
NervusRaccoon <56012846+NervusRaccoon@users.noreply.github.com>
2021-03-18 23:45:59 +0300

    added lab3

---
+++ RankCalculator/Program.cs
@@ -0,0 +1,47 @@
+using NATS.Client;
+using Valuator;
+using System;
+using System.Text;
+using System.Linq;
+using Valuator.Storage;
+
+namespace RankCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IStorage storage = new RedisStorage();
+            
+            Console.WriteLine("Consumer started");
+            ConnectionFactory cf = new ConnectionFactory();
+            using IConnection c = cf.CreateConnection();
+            var s = c.SubscribeAsync("valuator.processing.rank", "rank_calculator", (sender, args) =>
+            {
+                string id = Encoding.UTF8.GetString(args.Message.Data);
+                string textKey = Constants.textPref + id;
+                var text = storage.Load(textKey);
+                string rankKey = Constants.rankPref + id;
+                var rank = GetRank(text);
+                storage.Store(rankKey, rank.ToString());
+            });
+
+            s.Start();
+
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+
+            s.Unsubscribe();
+
+            c.Drain();
+            c.Close();
+        }
+        private static double GetRank(string text)
+        {
+            var count = text.Where(x => !Char.IsLetter(x)).Count();
+            double rank = (double) count / text.Count();
+
+            return rank;
+        }
+    }
+}
