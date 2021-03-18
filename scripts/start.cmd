32d5d0d51643c254054f122fb6723ba5adea7157
NervusRaccoon <56012846+NervusRaccoon@users.noreply.github.com>
2021-03-18 23:35:26 +0300

    added lab3

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -1,4 +1,7 @@
 start "localhost:5001" /d ..\Valuator\ dotnet run --no-build --urls "http://localhost:5001"
 start "localhost:5002" /d ..\Valuator\ dotnet run --no-build --urls "http://localhost:5002"
 
+start /d ..\RankCalculator\ dotnet run --no-build
+start /d ..\RankCalculator\ dotnet run --no-build
+
 start "nginx" /d ..\nginx\ nginx.exe
--- scripts/stop.cmd
+++ scripts/stop.cmd
@@ -1,2 +1,5 @@
+taskkill /f /IM rankCalculator.exe
 taskkill /f /IM valuator.exe
+
 cd "../nginx" & nginx.exe -s quit
+nginx -s stop
