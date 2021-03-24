5082979cdef8c88f2f5b8d6152f5b9e5535393b8
NervusRaccoon <56012846+NervusRaccoon@users.noreply.github.com>
2021-03-25 00:57:32 +0300

    delete Constants

--- Valuator/Constants.cs
+++
@@ -1,9 +0,0 @@
-namespace Valuator
-{
-    public static class Constants
-    {
-        public const string textPref = "TEXT-";
-        public const string rankPref = "RANK-";
-        public const string simPref = "SIMILARITY-";
-    }
-}
