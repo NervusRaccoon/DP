8c5666fb59be35b0634e7509bac7f21c1bc55862
NervusRaccoon <56012846+NervusRaccoon@users.noreply.github.com>
2021-06-08 21:42:29 +0300

    update start.cmd

--- scripts/start.cmd
+++ scripts/start.cmd
@@ -1,10 +1,17 @@
-start "localhost:5001" /d ..\Valuator\ dotnet run --urls "http://localhost:5001"
-start "localhost:5002" /d ..\Valuator\ dotnet run --urls "http://localhost:5002"
+cd ..\Valuator\
+dotnet build
+cd ..\RankCalculator\
+dotnet build
+cd ..\EventsLogger\
+dotnet build
 
-start "RankCalculator1" /d ..\RankCalculator\ dotnet run
-start "RankCalculator2" /d ..\RankCalculator\ dotnet run
+start "localhost:5001" /d ..\Valuator\ dotnet run --no-build --urls "http://localhost:5001"
+start "localhost:5002" /d ..\Valuator\ dotnet run --no-build --urls "http://localhost:5002"
 
-start "EventsLogger1" /d ..\EventsLogger\ dotnet run
-start "EventsLogger2" /d ..\EventsLogger\ dotnet run
+start "RankCalculator1" /d ..\RankCalculator\ dotnet run --no-build
+start "RankCalculator2" /d ..\RankCalculator\ dotnet run --no-build
+
+start "EventsLogger1" /d ..\EventsLogger\ dotnet run --no-build
+start "EventsLogger2" /d ..\EventsLogger\ dotnet run --no-build
 
 start "nginx" /d ..\nginx\ nginx.exe
